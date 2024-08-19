53c3a410cfcf448205b3f1255145b3b8d3747b96
Prayag <prayagbhatt2003@gmail.com>
2024-08-19 12:35:04 +0530

    feat(DB): migrate to TS, add Winston Logger

---
+++ src/db/conn.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+import { Logger as logger } from "../utils";
+
+interface ConnectionInstance {
+  connection: {
+    host: string;
+  };
+}
+
+const connectDB = async (): Promise<void> => {
+  try {
+    // Connection object in return
+    const connectionInstance: ConnectionInstance = (await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    )) as unknown as ConnectionInstance;
+
+    // NOTE: connectionInstance.connection.host is written since there can be many DBs, eg: for production, testing
+    logger.info(
+      `MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    // Ensure error is typed correctly
+    if (error instanceof Error) {
+      logger.error(`MongoDB connection failed: ${error.message}`);
+    } else {
+      logger.error(`MongoDB connection failed: ${error}`);
+    }
+
+    // NOTE: Process is the current application which is running
+    process.exit(1);
+  }
+};
+
+export default connectDB;
